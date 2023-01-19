2c3fb53345b75ba332f3b05165a7b7d0ff741d85
adriendst <70936701+adriendst@users.noreply.github.com>
2023-01-19 16:37:29 +0100

    ToDo List Basic

--- src/TodoListBasic/TodoListBasic.tsx
+++ src/TodoListBasic/TodoListBasic.tsx
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const TodoListBasic = () => {
-    return <div>TodoListBasic</div>;
+    const [toDoList, setToDoList] = useState<string[]>(['To do']);
+    const [inProgressList, setInProgressList] = useState<string[]>(['In progress']);
+    const [doneList, setDoneList] = useState<string[]>(['Done']);
+
+    const [task, setTask] = useState<string>('');
+    const [taskType, setTaskType] = useState<string>('');
+
+    const taskTypeList = ['To do', 'In Progress', 'Done'];
+
+    function handleChangeTask(e: React.ChangeEvent<HTMLInputElement>){
+        setTask(e.target.value);
+        console.log(task)
+    }
+
+    function handleChangeTaskType(e: React.ChangeEvent<HTMLSelectElement>){
+        setTaskType(e.target.value);
+    }
+
+    function addToList() {
+        if(taskType == 'To do') {
+            const newList = toDoList.concat(task);
+            setToDoList(newList);
+        }
+        else if(taskType == 'In Progress') {
+            const newList = inProgressList.concat(task);
+            setInProgressList(newList);
+        }
+        else {
+            const newList = doneList.concat(task);
+            setDoneList(newList);
+        }
+        setTask('')
+    }
+
+
+
+
+    return (<>
+        <div>
+            <input type={"text"} value={task} onChange={handleChangeTask}/>
+            <select value={taskType} onChange={handleChangeTaskType}>
+                <option>To do</option>
+                <option>In Progress</option>
+                <option>Done</option>
+            </select>
+            <button onClick={addToList}>Add to list</button>
+        </div>
+        <div>
+            <table>
+                <td style={{padding : '10px'}}>
+                    <ul>
+                        {toDoList.map((toDo) =>
+                            <li key={toDo}>{toDo}</li>
+                        )}
+                    </ul>
+                </td>
+                <td style={{padding : '10px'}}>
+                    <ul>
+                        {inProgressList.map((inProgress) =>
+                            <li key={inProgress}>{inProgress}</li>
+                        )}
+                    </ul>
+                </td>
+                <td style={{padding : '10px'}}>
+                    <ul>
+                        {doneList.map((done) =>
+                            <li key={done}>{done}</li>
+                        )}
+                    </ul>
+                </td>
+            </table>
+        </div>
+    </>)
 };
 
 export default TodoListBasic;
